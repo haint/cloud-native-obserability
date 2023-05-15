956152fbf74f126871a5d2a691cf2f8ccc600b16
Ethan Nguyen <ethan@timebird.org>
2023-05-16 00:54:35 +0700

    Update frontend

--- reference-app/frontend/static/js/click.js
+++ reference-app/frontend/static/js/click.js
@@ -3,14 +3,14 @@ $(document).ready(function () {
     // all custom jQuery will go here
     $("#firstbutton").click(function () {
         $.ajax({
-            url: "http://192.168.60.97:8081", success: function (result) {
+            url: "http://backend.default.svc.cluster.local", success: function (result) {
                 $("#firstbutton").toggleClass("btn-primary:focus");
                 }
         });
     });
     $("#secondbutton").click(function () {
         $.ajax({
-            url: "http://192.168.60.97:8082", success: function (result) {
+            url: "http://trial.default.svc.cluster.local", success: function (result) {
                 $("#secondbutton").toggleClass("btn-primary:focus");
             }
         });

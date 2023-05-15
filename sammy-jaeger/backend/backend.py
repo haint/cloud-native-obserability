f925478a1b95988a078f5df7cca7795cccee76e3
Ethan Nguyen <ethan@timebird.org>
2023-05-15 22:55:25 +0700

    Update service name

--- sammy-jaeger/backend/backend.py
+++ sammy-jaeger/backend/backend.py
@@ -16,7 +16,7 @@ config = Config(
         'logging': True,
         'reporter_batch_size': 1
     },
-    service_name = 'service'
+    service_name = 'service_backend'
 )
 jaeger_tracer = config.initialize_tracer()
 tracing = FlaskTracing(jaeger_tracer, True, app)
--- sammy-jaeger/frontend/frontend.py
+++ sammy-jaeger/frontend/frontend.py
@@ -14,7 +14,7 @@ config = Config(
         'logging': True,
         'reporter_batch_size': 1
     },
-    service_name = 'service'
+    service_name = 'service_frontend'
 )
 jaeger_tracer = config.initialize_tracer()
 tracing = FlaskTracing(jaeger_tracer, True, app)

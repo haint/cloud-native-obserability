0f8cd11857ab0a75630a59c66d579ab44f18a3e8
Ethan Nguyen <ethan@timebird.org>
2023-05-16 22:19:10 +0700

    Update frontend; backend tracing

--- reference-app/frontend/app.py
+++ reference-app/frontend/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
 
@@ -6,6 +7,16 @@ app = Flask(__name__)
 @app.route("/")
 def homepage():
     return render_template("main.html")
+
+@app.route("/backend")
+def backend():
+    response = requests.get("http://backend.default.svc.cluster.local:8081/")
+    return response
+
+@app.route("/trial")
+def trail():
+    response = requests.get("http://trial.default.svc.cluster.local:8082/")
+    return response
 
 
 if __name__ == "__main__":
--- reference-app/frontend/static/js/click.js
+++ reference-app/frontend/static/js/click.js
@@ -3,14 +3,14 @@ $(document).ready(function () {
     // all custom jQuery will go here
     $("#firstbutton").click(function () {
         $.ajax({
-            url: "http://backend.default.svc.cluster.local:8081", success: function (result) {
+            url: "/backend", success: function (result) {
                 $("#firstbutton").toggleClass("btn-primary:focus");
                 }
         });
     });
     $("#secondbutton").click(function () {
         $.ajax({
-            url: "http://trial.default.svc.cluster.local:8082", success: function (result) {
+            url: "/trial", success: function (result) {
                 $("#secondbutton").toggleClass("btn-primary:focus");
             }
         });
